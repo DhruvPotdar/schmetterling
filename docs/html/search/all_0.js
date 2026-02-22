a5969281840524740ef0d1998bb13fba6df7fefb
DhruvPotdar <dhruv.potdar@atimotors.com>
2026-02-22 16:10:11 +0530

    removed docs

--- docs/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['_5fbits_0',['_bits',['../db/d0e/classBitBoard.html#a5643eb23de186d63e0426e545094aa12',1,'BitBoard']]],
-  ['_5fboard_1',['_board',['../d0/dab/classMoveGenerator.html#ac9f3cdeec2318ec5369b41d2a101c27d',1,'MoveGenerator']]]
-];
